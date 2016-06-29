2f8817d55d84096e4928256f2b4b3e4be94beccf
bigrig2212 <bigrig2212@users.noreply.github.com>
2016-06-29 08:24:44 +0100

    Add 100 boids

--- sketch.js
+++ sketch.js
@@ -36,7 +36,7 @@ function setup() {
   slider_variance   = createSlider(0, 50, 6).parent('variance');
   
     // We are now making random vehicles and storing them in an array
-  for (var i = 0; i < 50; i++) {
+  for (var i = 0; i < 100; i++) {
     vehicles.push(new Vehicle(random(width),random(height)));
   }
 }
