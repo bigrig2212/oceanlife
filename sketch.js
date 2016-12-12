40cf07fc323e788dd03544d09c3f3184de609b83
benrigby <ben@415.com>
2016-12-12 13:17:59 -0800

    Update to most recent

--- sketch.js
+++ sketch.js
@@ -13,48 +13,160 @@ wavestarts[1].color = '#9999cc';
 wavestarts[2] = {};
 wavestarts[2].difference = -29; //difference between wave2 and original wave
 wavestarts[2].variance = 4; //variance
-wavestarts[2].color = 'red'; 
+wavestarts[2].color = '#cccccc'; 
 
 wavestarts[3] = {};
 wavestarts[3].difference = -46; //difference between wave2 and original wave
 wavestarts[3].variance = 4; //variance
-wavestarts[3].color = 'green'; 
+wavestarts[3].color = '#666666'; 
 
 wavestarts[4] = {};
 wavestarts[4].difference = -56; //difference between wave2 and original wave
 wavestarts[4].variance = 4; //variance
-wavestarts[4].color = 'blue'; 
+wavestarts[4].color = '#666699'; 
 
 // A list of vehicles
 var vehicles = [];
 
+// "x-offset" in Perlin noise space for fish
+var fish_xoff = 0;
+var fish_x = 0;
+var fish_y = 0;
+var framecount = 0;
+
+var canvas_width = 700;
+var canvas_height = 600;
+
+//"rockets"
+var lifetime;  // How long should each generation live
+var population;  // Population
+var lifeCounter;   // Timer for cycle of generation
+var target;        // Target location
+var info;
+var recordtime;         // Fastest time to target
+var obstacles;  //an array list to keep track of all the obstacles!
+
+//images
+var ft = {}; //namespace
+
+
+function preload() {
+  ft.lighthouse = loadImage('assets/lighthouse.png'); 
+  ft.rocks = loadImage('assets/rocks2.png');
+  ft.sky = loadImage('assets/sky.png');
+  ft.water = loadImage('assets/water.png');
+  ft.fish = loadImage('assets/fish2.png');
+}
+
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(canvas_width, canvas_height);
   slider_start2   = createSlider(0, 800, 300).parent('start2');
   slider_stop2   = createSlider(0, 800, 280).parent('stop2');
   slider_difference   = createSlider(-100, 100, -15).parent('difference');
   slider_variance   = createSlider(0, 50, 6).parent('variance');
   
-    // We are now making random vehicles and storing them in an array
-  for (var i = 0; i < 100; i++) {
+  // We are now making random vehicles and storing them in an array
+  for (var i = 0; i < 75; i++) {
     vehicles.push(new Vehicle(random(width),random(height)));
   }
+  
+  //"rockets"
+  // The number of cycles we will allow a generation to live
+  lifetime = height/2;
+  // Initialize variables
+  lifeCounter = 0;
+  target = new Obstacle(width, 24, 24, 24);
+
+  // Create a population with a mutation rate, and population max
+  var mutationRate = 0.01;
+  population = new Population(mutationRate, 50);
+  info = createP("");
+  info.position(250,10);
+  recordtime = lifeCounter;
+  target = new Obstacle(width/2-12, 24, 25, 25);
+  // Create the obstacle course
+  obstacles = [];
+  obstacles.push(new Obstacle(0, 300, 800, 10));
 }
 
 function draw() {
   background('#cccccc');
+  
+  image(ft.sky, 0, 0);
+  image(ft.lighthouse, 400, 130);
   drawwaves();
+  image(ft.water, 0, 293);
+  image(ft.rocks, 162, 465);
+ 
+  set_new_seek_target();
+  draw_otherfish();
+  draw_learningfish()
+}
+
+//------------------------------------------------------
+function draw_otherfish(){
+  //sets globals used in next for loop
+  
   
   for (var i = 0; i < vehicles.length; i++) {
-    vehicles[i].applyBehaviors(vehicles);
+    vehicles[i].applyBehaviors(vehicles, fish_x, fish_y);
     vehicles[i].update();
     vehicles[i].borders();
     vehicles[i].display(); 
   }
- 
+}
+function draw_learningfish(){
+  
+  // Draw the start and target locations
+  target.display();
+
+  // If the generation hasn't ended yet
+  if (lifeCounter < lifetime) {
+    population.live(obstacles);
+    if ((population.targetReached()) && (lifeCounter < recordtime)) {
+      recordtime = lifeCounter;
+    }
+    lifeCounter++;
+    // Otherwise a new generation
+  }
+  else {
+    lifeCounter = 0;
+    population.fitness();
+    population.selection();
+    population.reproduction();
+  }
+
+ // Draw the obstacles
+  for (var i = 0; i < obstacles.length; i++) {
+    obstacles[i].display();
+
+  }
+  //info.html("Generation #: " + population.getGenerations() + "<br>" + "Cycles left: " + (lifetime-lifeCounter));
+
 }
 
-//------------------------------------------------------
+
+//every N frames, set a new seek target 
+function set_new_seek_target(){
+  var n = 500;
+  if (framecount%n === 0){
+    fish_x = noise(fish_xoff) * width;
+    fish_xoff += 0.1;
+    fish_y = random(canvas_height-200, canvas_height-150);
+  }
+  framecount++;
+  
+  //debug, show target
+  push();
+  fill('red');
+  //ellipse(fish_x, fish_y, 25, 25);
+  pop();
+  
+  //set learning target
+  target.location.x = fish_x;
+  target.location.y = fish_y;
+}
+
 function drawwaves(){
    //play around with having slider affect only one wave
    //if i add it to all waves, they all behave too similarly
--- vehicle.js
+++ vehicle.js
@@ -8,32 +8,33 @@
 function Vehicle(x, y) {
   // All the usual stuff
   this.position = createVector(x, y);
-  this.r = 12;
+  this.r = 7;
   this.maxspeed = 3;    // Maximum speed
   this.maxforce = 0.2;  // Maximum steering force
   this.acceleration = createVector(0, 0);
   this.velocity = createVector(0, 0);
+}
 
-  this.applyBehaviors = function(vehicles) {
-
+Vehicle.prototype.applyBehaviors = function(vehicles,seekx,seeky) {
      var separateForce = this.separate(vehicles);
-     var seekForce = this.seek(createVector(mouseX,mouseY));
-
+     var seekForce = this.seek(createVector(seekx, seeky));
+     //var seekForce = this.seek(createVector(mouseX, mouseY));
+     
      separateForce.mult(2);
      seekForce.mult(1);
 
      this.applyForce(separateForce);
      this.applyForce(seekForce);
-  }
+}
 
-  this.applyForce = function(force) {
+Vehicle.prototype.applyForce = function(force) {
     // We could add mass here if we want A = F / M
     this.acceleration.add(force);
-  }
+}
 
   // Separation
   // Method checks for nearby vehicles and steers away
-  this.separate = function(vehicles) {
+Vehicle.prototype.separate = function(vehicles) {
     var desiredseparation = 20;
     var sum = createVector();
     var count = 0;
@@ -61,11 +62,11 @@ function Vehicle(x, y) {
       sum.limit(this.maxforce);
     }
     return sum;
-  }
+}
 
   // A method that calculates a steering force towards a target
   // STEER = DESIRED MINUS VELOCITY
-  this.seek = function(target) {
+Vehicle.prototype.seek = function(target) {
     var desired = p5.Vector.sub(target,this.position);  // A vector pointing from the location to the target
 
     // Normalize desired and scale to maximum speed
@@ -75,10 +76,10 @@ function Vehicle(x, y) {
     var steer = p5.Vector.sub(desired,this.velocity);
     steer.limit(this.maxforce);  // Limit to maximum steering force
     return steer;
-  }
+}
 
   // Method to update location
-  this.update = function() {
+Vehicle.prototype.update = function() {
     // Update velocity
     this.velocity.add(this.acceleration);
     // Limit speed
@@ -86,9 +87,9 @@ function Vehicle(x, y) {
     this.position.add(this.velocity);
     // Reset accelertion to 0 each cycle
     this.acceleration.mult(0);
-  }
+}
 
-  this.display = function() {
+Vehicle.prototype.display = function() {
     fill(127);
     stroke(200);
     strokeWeight(2);
@@ -96,13 +97,13 @@ function Vehicle(x, y) {
     translate(this.position.x, this.position.y);
     ellipse(0, 0, this.r, this.r);
     pop();
-  }
+}
 
+Vehicle.prototype.borders = function() {
   // Wraparound
-  this.borders = function() {
     if (this.position.x < -this.r) this.position.x =  width+this.r;
-    if (this.position.y < -this.r) this.position.y = height+this.r;
+    if (this.position.y < (height/2)-this.r) this.position.y = this.position.y - 10; //this is a really cool effect - jumping skywards
+    //if (this.position.y < (height/2)-this.r) this.position.y = (height/2)-this.r + random(1,5); //keeps the vehicles in the water
     if (this.position.x >  width+this.r) this.position.x = -this.r;
     if (this.position.y > height+this.r) this.position.y = -this.r;
-  }
 }
